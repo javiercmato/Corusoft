50a974814188e5831b18821ec8796fc3a1448341
Juan <j.justo.armesto@udc.es>
2023-03-23 15:34:48 +0100

    [mobile] Added log messages and Intent transitions to Landing page

--- mobile/app/src/main/java/com/corusoft/ticketmanager/LogIn.kt
+++ mobile/app/src/main/java/com/corusoft/ticketmanager/LogIn.kt
@@ -1,6 +1,9 @@
 package com.corusoft.ticketmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class LogIn : AppCompatActivity() {
@@ -8,4 +11,25 @@ class LogIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
     }
+
+    fun logIn(view: View) {
+        Log.d("logIn", "Button log in")
+        // ToDo: username/email and password
+        val intent = Intent(this, Landing::class.java)
+        startActivity(intent)
+    }
+
+    fun logInGoogle(view: View) {
+        Log.d("logInGoogle", "Button log in google")
+        // ToDo: username/email and password
+        val intent = Intent(this, Landing::class.java)
+        startActivity(intent)
+    }
+
+    fun signUp(view: View) {
+        Log.d("signUp", "Button sign up")
+        val intent = Intent(this, SignUp::class.java)
+        startActivity(intent)
+    }
+
 }
--- mobile/app/src/main/java/com/corusoft/ticketmanager/SignUp.kt
+++ mobile/app/src/main/java/com/corusoft/ticketmanager/SignUp.kt
@@ -1,6 +1,9 @@
 package com.corusoft.ticketmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUp : AppCompatActivity() {
@@ -8,4 +11,18 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
+
+    fun signUp(view: View) {
+        Log.d("signUp", "Button sign up")
+        // ToDo: username/email, password, ...
+        val intent = Intent(this, Landing::class.java)
+        startActivity(intent)
+    }
+
+    fun logIn(view: View) {
+        Log.d("logIn", "Button log in")
+        val intent = Intent(this, LogIn::class.java)
+        startActivity(intent)
+    }
+
 }
