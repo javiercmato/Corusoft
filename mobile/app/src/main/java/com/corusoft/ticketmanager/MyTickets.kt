4079036ca23bfba256cfe492d617cb52c0def39a
Lucas <lucaslara@correo.ugr.es>
2023-04-21 17:43:25 +0200

    Corroutine implemented in MyTickets screen cases

--- mobile/app/src/main/java/com/corusoft/ticketmanager/MyTickets.kt
+++ mobile/app/src/main/java/com/corusoft/ticketmanager/MyTickets.kt
@@ -6,11 +6,15 @@ import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.*
+import android.widget.Toast
 
 class MyTickets : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_tickets)
+
+        datosCorrutina()
 
         //Obtener referencia al botón
         val filterButton = findViewById<Button>(R.id.buttonFilter)
@@ -24,5 +28,15 @@ class MyTickets : AppCompatActivity() {
             val intent = Intent(this, MyTicketsFilter::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun datosCorrutina() = runBlocking {
+        coroutineScope {
+            launch {
+                delay(1000L)
+                println("Solicitando listado de tickets...")
+            }
+        }
+        println("Listado de tickets recibidos")
     }
 }
