506ea3804c93d3658c934df455127fe0221cdaa1
Manu Corujo <manucorujo@gmail.com>
2023-04-21 17:31:48 +0200

    [mobile] Implemented corroutine in Landing screen cases

--- mobile/app/src/main/java/com/corusoft/ticketmanager/Landing.kt
+++ mobile/app/src/main/java/com/corusoft/ticketmanager/Landing.kt
@@ -4,11 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.*
+import android.widget.Toast
+
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
+        datosCorrutina()
+
     }
+    private fun datosCorrutina() = runBlocking {
+        coroutineScope {
+            launch {
+                delay(1000L)
+                println("Solicitando datos históricos a backend...")
+            }
+        }
+        println("Datos históricos recibidos")
+        Toast.makeText(applicationContext, "Datos históricos recibidos", Toast.LENGTH_SHORT)
+            .show()
+    }
+
 }
