468fc37b1dd6672a087fa1dd8fece3c413b07935
Juan <j.justo.armesto@udc.es>
2023-05-07 12:46:14 +0200

    [mobile] Toast when password and repeat password not equals

--- mobile/app/src/main/java/com/corusoft/ticketmanager/SignUp.kt
+++ mobile/app/src/main/java/com/corusoft/ticketmanager/SignUp.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.corusoft.ticketmanager.backend.BackendAPI
 import com.corusoft.ticketmanager.backend.dtos.users.RegisterUserParamsDTO
@@ -26,31 +27,37 @@ class SignUp : AppCompatActivity() {
         val nickname = findViewById<EditText>(R.id.editTextTextPersonName5).text.toString()
         val email = findViewById<EditText>(R.id.editTextTextPersonName6).text.toString()
         val password = findViewById<EditText>(R.id.editTextTextPersonName7).text.toString()
+        val repeatPassword = findViewById<EditText>(R.id.editTextTextPersonName8).text.toString()
         val params = RegisterUserParamsDTO(nickname, password, firstName + lastName, email)
 
-        try {
-            coroutineScope {
-                launch {
-                    var success = false;
-                    try {
-                        val response = backend.signUp(params)
-                        if (response != null) {
-                            success = true
-                            Log.i("SignUp", "Signup user '${response.nickname}' success")
+        if (password == repeatPassword) {
+            try {
+                coroutineScope {
+                    launch {
+                        var success = false;
+                        try {
+                            val response = backend.signUp(params)
+                            if (response != null) {
+                                success = true
+                                Log.i("SignUp", "Signup user '${response.nickname}' success")
+                            }
+                        } catch (ex: Exception) {
+                            success = false
                         }
-                    } catch (ex: Exception) {
-                        success = false
-                    }
 
-                    if (success) {
-                        val intent = Intent(this@SignUp, Landing::class.java)
-                        startActivity(intent)
+                        if (success) {
+                            val intent = Intent(this@SignUp, Landing::class.java)
+                            startActivity(intent)
+                        }
                     }
                 }
+            } catch (ex: Exception) {
+                System.err.println(ex.localizedMessage)
             }
-        } catch (ex: Exception) {
-            System.err.println(ex.localizedMessage)
+        } else {
+            Toast.makeText(this@SignUp, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
         }
+
     }
 
     fun logIn(view: View) {
