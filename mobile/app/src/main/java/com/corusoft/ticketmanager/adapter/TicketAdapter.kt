8978ee017b6f5b3b655ed5e2d169df871e259e7a
Juan <j.justo.armesto@udc.es>
2023-05-23 11:21:12 +0200

    [mobile] added image to ticket

--- mobile/app/src/main/java/com/corusoft/ticketmanager/adapter/TicketAdapter.kt
+++ mobile/app/src/main/java/com/corusoft/ticketmanager/adapter/TicketAdapter.kt
@@ -1,9 +1,13 @@
 package com.corusoft.ticketmanager.adapter
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -21,6 +25,7 @@ class TicketAdapter(
         val storeView: TextView = view.findViewById(R.id.textView13)
         val currencyView: TextView = view.findViewById(R.id.textView9)
         val nameView: TextView = view.findViewById(R.id.name)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
         val expandableLayout: RelativeLayout = view.findViewById(R.id.expandable_layout)
         val notexpandable_layout: LinearLayout = view.findViewById(R.id.notexpandable_layout)
     }
@@ -36,6 +41,7 @@ class TicketAdapter(
         holder.storeView.text = ticket.store
         holder.currencyView.text = ticket.currency
         holder.nameView.text = ticket.name
+        holder.imageView.setImageBitmap(decodeBase64(ticket.picture))
 
         val isExpandable = dataset[position].expandable
         holder.expandableLayout.visibility = if (isExpandable) View.VISIBLE else View.GONE
@@ -44,8 +50,21 @@ class TicketAdapter(
             val ticket = dataset[position]
             ticket.expandable = !ticket.expandable
             notifyItemChanged(position)
+            holder.imageView.height
         }
     }
 
     override fun getItemCount() = dataset.size
+
+    private fun decodeBase64(base64String: String?) : Bitmap {
+
+        // Decodifica la cadena Base64 a un array de bytes
+        val decodeBytes = Base64.decode(base64String, Base64.DEFAULT)
+
+        // Crea un objeto Bitmap a partir del array de bytes
+        val bitmap = BitmapFactory.decodeByteArray(decodeBytes, 0, decodeBytes.size)
+
+        return bitmap
+    }
+
 }
